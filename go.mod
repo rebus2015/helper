ecdc5dcbbbe7bd45c309bc0fdffb24764bad3e68
mak <pablo_57@mail.ru>
2024-11-17 16:54:23 +0300

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/rebus2015/helper
+
+go 1.21.1
---
+++ helper.go
@@ -0,0 +1,12 @@
+package helper
+
+import "fmt"
+
+func Action(a int, b int) int {
+	return a + b
+}
+
+// Hi returns a friendly greeting
+func Hi(name string) string {
+	return fmt.Sprintf("Hi, %s", name)
+}
